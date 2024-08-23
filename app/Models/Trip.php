e6502f9ee25dec8f243ddb93191f502a0008148a
MarioBroi <inoix16@gmail.com>
2024-08-23 13:19:38 +0200

    add fillables to Place and Trip models

--- app/Models/Trip.php
+++ app/Models/Trip.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Trip extends Model
 {
     use HasFactory;
+
+    protected $fillable = ["title", "desciption", "start_date", "end_date"];
+
+    public function places()
+    {
+        return $this->hasMany(Place::class);
+    }
 }
